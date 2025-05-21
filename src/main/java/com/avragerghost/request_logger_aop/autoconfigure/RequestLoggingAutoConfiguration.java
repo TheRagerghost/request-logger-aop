663c15c3382c4321cc7683dd7d31e942153a9310
Ragerghost <avragerghost@gmail.com>
2025-05-21 19:20:35 +0300

    mod: basic starter setup

---
+++ src/main/java/com/avragerghost/request_logger_aop/autoconfigure/RequestLoggingAutoConfiguration.java
@@ -0,0 +1,9 @@
+package com.avragerghost.request_logger_aop.autoconfigure;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@AutoConfiguration
+@EnableAspectJAutoProxy
+public class RequestLoggingAutoConfiguration {
+}
